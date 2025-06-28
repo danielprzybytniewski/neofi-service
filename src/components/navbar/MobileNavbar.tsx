b9ddaaab791d78d50c2c5d24960c6d36eb81ecd3
danielprzybytniewski <danielprzybytniewski@gmail.com>
2025-06-28 06:42:20 +0200

    feat: add fixed navbar

--- src/components/navbar/MobileNavbar.tsx
+++ src/components/navbar/MobileNavbar.tsx
@@ -11,16 +11,24 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { useScrollDetection } from "@/hooks/useScrollDetection";
+import { cn } from "@/lib/utils";
 
 export default function MobileNavbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const isScrolled = useScrollDetection();
 
   const handleClose = () => {
     setIsOpen(false);
   };
 
   return (
-    <nav className="h-18 p-5 lg:hidden">
+    <nav
+      className={cn(
+        "bg-background fixed top-0 left-0 z-50 h-18 w-full p-5 lg:hidden",
+        isScrolled && "shadow-lg",
+      )}
+    >
       <div className="flex h-full items-center justify-between">
         <a href="#hero">
           <img src={logoImg} alt="NeoFi Logo" />
