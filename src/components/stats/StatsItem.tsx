88f70f1e536ff6b35ea2cedcae114cec288461b9
danielprzybytniewski <danielprzybytniewski@gmail.com>
2025-07-14 11:30:57 +0200

    feat: add animations for stats-section

--- src/components/stats/StatsItem.tsx
+++ src/components/stats/StatsItem.tsx
@@ -1,16 +1,29 @@
+import { motion } from "framer-motion";
 import Typography from "@/components/shared/Typography";
 import type { Stat } from "@/constants/stats";
 
 export default function StatsItem({ value, label }: Stat) {
   return (
     <div className="flex flex-col items-center">
-      <Typography
-        component="h3"
-        variant="title"
-        className="text-secondary mb-3 text-5xl lg:mb-5"
+      <motion.div
+        animate={{ scale: 1.05 }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "mirror",
+          duration: 2,
+          type: "spring",
+          stiffness: 100,
+          damping: 30,
+        }}
       >
-        {value}
-      </Typography>
+        <Typography
+          component="h3"
+          variant="title"
+          className="text-secondary mb-3 text-5xl lg:mb-5"
+        >
+          {value}
+        </Typography>
+      </motion.div>
       <Typography variant="subtitle" className="text-xl">
         {label}
       </Typography>
